a0fc303b39128d3e636bddfe60839bcbcb2ad0b3
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2022-11-27 01:37:57 -0500

    bug fix

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,7 +1,7 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
-import * as fromAuth from '../auth/store/add.reducer';
+import * as fromAuth from '../auth/store/auth.reducer';
 
 export interface AppState {
     shoppingList: fromShoppingList.State;
