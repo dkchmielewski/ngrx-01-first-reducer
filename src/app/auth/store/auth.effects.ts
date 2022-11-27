81db6c5e01567cd8e85f90e8a57e2b3028cb51c1
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2022-11-27 01:12:27 -0500

    login via NgRx effects

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@angular/core';
 import { Actions, ofType, Effect } from '@ngrx/effects';
 import { switchMap, catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
@@ -16,6 +17,7 @@ export interface AuthResponseData {
     registered?: boolean;
   }
 
+@Injectable()
 export class AuthEffects {
     @Effect()
     authLogin = this.actions$.pipe(
@@ -30,12 +32,22 @@ export class AuthEffects {
                     returnSecureToken: true
                 }
             ).pipe(
-                catchError(error => {
-                //...
-                of();
-                }), 
                 map(resData => {
-                    of();
+                    const expirationDate = new Date(
+                        new Date().getTime() + +resData.expiresIn * 1000
+                    );
+                    return of(
+                        new AuthActions.Login({
+                            email: resData.email, 
+                            userId: resData.localId, 
+                            token: resData.idToken, 
+                            expirationDate: expirationDate
+                        })
+                    );
+                }),
+                catchError(error => {
+                    //...
+                    return of();
                 })
             );
         }),
